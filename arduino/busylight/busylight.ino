8fa816f57e4ced6482508775df8e324777f622eb
Steve Willoughby <steve@madscience.zone>
2023-10-25 18:17:18 -0700

    updated command interpretation and RS-485 protocol

--- arduino/busylight/busylight.ino
+++ arduino/busylight/busylight.ino
@@ -1,6 +1,6 @@
 // Change the following PER DEVICE before flashing.
 #define THIS_DEVICE_COLOR_MAP "BRrYG"
-#define SERIAL_VERSION_STAMP "V1.0.2$R2.0.0$SB001$"
+#define SERIAL_VERSION_STAMP "V1.0.2$R2.0.0$SBXXX$"
 //                             \___/  \___/  \__/
 //                               |      |      |
 //                  Hardware version    |      |
@@ -8,6 +8,12 @@
 //                                 Serial number
 //
 // serial numbers B000-B299 reserved for author's use
+
+// Change this to indicate if the RS-485 and USB inputs share a
+// common USART (true for the Arduino Nano board this project was
+// originally designed for), or if the USB input is on Serial and
+// the RS-485 is on Serial1.
+#define COMMON_SERIAL_PORT (true)	/* true=shared port (Uno, Nano); false=separate ports (Mega) */
 
 /*
 ** Steve Willoughby <steve@madscience.zone>
@@ -50,6 +56,24 @@
 ** occurs, all data are ignored until a ^D is received. Extra data
 ** after a command (but before the terminating ^D) is ignored.
 **
+** Alternatively, commands received via RS-485 start with a binary
+** header to be compatible with Lumos devices on the same network.
+**
+** 1000aaaa                                              turn all LEDs off. [1]
+** 1001aaaa <command>                                    do <command> as described below. [1][3]
+** 1011aaaa 0000nnnn 0000xxxx ... 0000xxxx <command>     do <command> as described below. [2][3]
+**                   \________<n>________/
+**
+** [1] if aaaa is the device address or the global address, the unit will
+**     respond to the command.
+**
+** [2] aaaa must be the global address; if the unit address is in the
+**     list of addresses following the start byte, it will respond.
+**
+** [3] All following bytes must have MSB=0 and use the escape sequences:
+**       01111110 0xxxxxxx     -> 1xxxxxxx (set MSB in next byte)
+**       01111111 0xxxxxxx     -> 0xxxxxxx (take next byte literally)
+**
 ** * <L0> <L1> ... <Ln-1> $     strobe LEDs
 ** = __ <spd>                   set baud rate to <spd>
 ** ?                            query LED status
@@ -83,33 +107,36 @@
 **     +            change transition and then re-examine current input byte
 **                          
 **                    
-**                    
-**                    
-**  _____     ^D        ________ 
-** |ERROR|----------->||Idle    ||
-** |_____|<-----------||________||
-**   ^ |       *            |
-**   |_|*                   |                              _
-//          ?/Q             |            ____             | |*
-//  END <-------------------+ '*'   ____|_   | led       _V_|_  ^D
-//                          +----->|Strobe|<-+      $   |END  |-----------> Idle
-//   _____    =             |      |______|------------>|_____|
-//  |Set  |<----------------+ 
-//  |_____|                 |
-//    | *                   |
-//   _V_____                |
-//  |SetUspd|               |
-//  |_______|               |
-//    | speed               |
-//    V                     |           ____
-//   END                    |  F    ___|_   |led
-//                          +----->|Flash|<-+      $
-//                          |      |_____|----------> END
-//                          |  S    ________ led
-//                          +----->|LightSet|----------> END
-//                          |      |________| 
-//                          |  X
-//                          +-----> END
+**                                                                             ____ 
+**          $8a/$8g (485) _      $Bg (485)        _________  N    ____________|_   | Ad[0]-Ad[N-2]
+**                       | |  +----------------->|Collect N|---->|CollectAddress|<-+
+**        MSB=1 (485)+   | |  |                  |_________|     |______________|
+**  _____  ^D (USB)     _|_V__|_   $9a/$Dg (485)  _____             |Ad[N-1]
+** |ERROR|----------->||Idle    ||-------------->|Start|<-----------+
+** |_____|<-----------||________||               |_____|
+**   ^ |       *            |                        |
+**   |_|*                   |<-----------------------+     _ 
+**          ?/Q             |            ____             | |*
+**  END <-------------------+ '*'   ____|_   | led       _V_|_  ^D
+**                          +----->|Strobe|<-+      $   |END  |-----------> Idle
+**   _____    =             |      |______|------------>|_____|
+**  |Set  |<----------------+ 
+**  |_____|                 |
+**    | Ad/_                |
+**   _V_____                |
+**  |SetUspd|               |
+**  |_______|               |
+**    | speed               |
+**   _V_____                |           ____
+**  |SetRspd|               |  F    ___|_   |led
+**  |_______|               +----->|Flash|<-+      $
+**    | speed               |      |_____|----------> END 
+**   _V_____                |  S    ________ led
+**  |SetAg  |               +----->|LightSet|----------> END
+**  |_______|               |      |________| 
+**    | addr                |  X
+**    V                     +-----> END
+**   END
 **
 ** Responses:
 ** ?
@@ -133,12 +160,19 @@
 //
 // EEPROM locations
 // $00 0x4B
-// $01 baud rate code
+// $01 baud rate code (USB)
+// $02 baud rate code (RS-485 if on separate USART) or 0 if port disabled
+// $03 unit address or UNIT_DISABLED
+// $04 global address or UNIT_DISABLED
 //
-#define EE_ADDR_SENTINEL  (0x00)
-#define EE_ADDR_USB_SPEED (0x01)
-#define EE_VALUE_SENTINEL (0xb1)
-#define EE_DEFAULT_SPEED  ('5')
+#define EE_ADDR_SENTINEL  (0x00) /* EEPROM $00: constant sentinel byte */
+#define EE_ADDR_USB_SPEED (0x01) /* EEPROM $01: usb baud rate code */
+#define EE_ADDR_485_SPEED (0x02) /* EEPROM $02: rs-485 baud rate code; NOT USED if common serial port */
+#define EE_ADDR_485_ADDR  (0x03) /* EEPROM $03: rs-485 unit address */
+#define EE_ADDR_GLOB_ADDR (0x04) /* EEPROM $04: rs-485 global address */
+#define EE_VALUE_SENTINEL (0xb1) /* EEPROM sentinal byte value */
+#define EE_DEFAULT_SPEED  ('5')  /* default baud rate code */
+#define UNIT_DISABLED     (0xff) /* address code if unit unit has no address */
 
 //
 // Digital output pin numbers for the lights
@@ -153,8 +187,8 @@ const int tree_port[] = {10, 7, 6, 8, 9, 14, 16};
 // at each flash.
 // 
 #define MAX_SEQUENCE    (64)
-#define SEQUENCE_ERROR (254)	// error in sequence codes
-#define SEQUENCE_OFF   (255)    // spot in sequence where LEDs are off
+#define SEQUENCE_ERROR (254)	/* error in sequence codes */
+#define SEQUENCE_OFF   (255)    /* spot in sequence where LEDs are off */
 class LightBlinker {
 	unsigned int on_period;	  // in mS 
 	unsigned int off_period;  // in mS
@@ -316,21 +350,30 @@ const int CSM_BUFSIZE = 64;
 class CommandStateMachine {
 private:
 	enum StateCode {
-		IdleState,
+		CollectAddressState,
+		CollectNState,
+		EndState,
 		ErrorState,
 		FlashState,
+		IdleState,
 		LightSetState,
-		StrobeState,
+		SetAgState,
 		SetState,
+		SetRSpeedState,
 		SetUSpeedState,
-		EndState,
+		StartState,
+		StrobeState,
 	} state;
 	byte LEDset;
 	byte command_in_progress;
 	byte buffer[CSM_BUFSIZE];
+	byte address_count;
 	byte buffer_idx;
+	bool source_485;
+	bool next_byte_msb;
+	bool next_byte_literal;
 public:
-	void accept(int inputchar);
+	void accept(int inputchar, bool from_485=false);
 	bool accept_led_name(int inputchar);
 	bool append_buffer(byte value);
 	void begin(void);
@@ -353,6 +396,8 @@ void strobe_lights(void);
 LightBlinker flasher(200, 0, flash_lights);
 LightBlinker strober(50, 2000, strobe_lights);
 CommandStateMachine csm;
+byte my_address = UNIT_DISABLED;
+byte global_address = UNIT_DISABLED;
 
 void setup() {
 	int i=0;
@@ -377,6 +422,11 @@ void setup() {
 	strober.stop();
 	digitalWrite(tree_port[0], HIGH);
 	start_usb_serial();
+	my_address = EEPROM.read(EE_ADDR_485_ADDR);
+	global_address = EEPROM.read(EE_ADDR_GLOB_ADDR);
+	if (my_address != UNIT_DISABLED) {
+		start_485_serial();
+	}
 	digitalWrite(tree_port[0], LOW);
 	csm.begin();
 }
@@ -385,6 +435,9 @@ void default_eeprom_settings(void) {
 	if (EEPROM.read(EE_ADDR_SENTINEL) != EE_VALUE_SENTINEL) {
 		// apparently unset; set to "factory defaults"
 		EEPROM.write(EE_ADDR_USB_SPEED, EE_DEFAULT_SPEED);
+		EEPROM.write(EE_ADDR_485_SPEED, 0); // 0=disabled
+		EEPROM.write(EE_ADDR_485_ADDR, UNIT_DISABLED);
+		EEPROM.write(EE_ADDR_GLOB_ADDR, UNIT_DISABLED);
 		EEPROM.write(EE_ADDR_SENTINEL, EE_VALUE_SENTINEL);
 		return;
 	}
@@ -398,10 +451,10 @@ void default_eeprom_settings(void) {
 	}
 }
 
-void start_usb_serial(void) {
+long decode_baud_rate(byte code) {
 	long speed = 9600;
 
-	switch (EEPROM.read(EE_ADDR_USB_SPEED)) {
+	switch (code) {
 		case '0': speed =    300; break;
 		case '1': speed =    600; break;
 		case '2': speed =   1200; break;
@@ -419,9 +472,24 @@ void start_usb_serial(void) {
 		case 'c':
 		case 'C': speed = 115200; break;
 	}
+	return speed;
+}
 
+void start_usb_serial(void) {
+	long speed = decode_baud_rate(EEPROM.read(EE_ADDR_USB_SPEED));
 	Serial.begin(speed);
 	while (!Serial);
+}
+
+void start_485_serial(void) {
+#if !COMMON_SERIAL_PORT
+	byte speed_code = EEPROM.read(EE_ADDR_485_SPEED);
+	if (speed_code > 0) {
+		long speed = decode_baud_rate(speed_code);
+		Serial1.begin(speed);
+		while (!Serial1);
+	}
+#endif
 }
 
 //
@@ -459,20 +527,39 @@ void report_LED_state(void)
 
 void report_device_state(void)
 {
+	byte b;
 	Serial.write('Q');	// query response
 	Serial.write('B');	// device model = busylight
 	Serial.write('=');	// settings
-	Serial.write('_');	// not implemented for this device
+	if (my_address == UNIT_DISABLED) {
+		Serial.write('_');
+	} else if (my_address < 16) {
+		Serial.write(my_address + '0');
+	} else {
+		Serial.write('?');
+	}
 	Serial.write(EEPROM.read(EE_ADDR_USB_SPEED)); // USB speed
-	Serial.write('_');	// not implemented for this device
-	Serial.write('_');	// not implemented for this device
+#if COMMON_SERIAL_PORT
+	Serial.write(EEPROM.read(EE_ADDR_USB_SPEED)); // USB speed
+#else
+	if ((b = EEPROM.read(EE_ADDR_485_SPEED)) == 0) {
+		Serial.write('_');
+	} else {
+		Serial.write(b);
+	}
+#endif
+	if (global_address == UNIT_DISABLED) {
+		Serial.write('_');
+	} else if (global_address < 16) {
+		Serial.write(global_address + '0');
+	} else {
+		Serial.write('?');
+	}
 	Serial.write('$');	// end of settings
 	Serial.write(SERIAL_VERSION_STAMP);	// hardware version, firmware version, serial number
 	Serial.write('\n'); // end of query response
 }
-	
 
-	
 bool set_baud_rate(byte baud_code) {
 	if ((baud_code >= '0' && baud_code <= '9') 
 	|| (baud_code >= 'A' && baud_code <= 'C')
@@ -484,15 +571,39 @@ bool set_baud_rate(byte baud_code) {
 	return false;
 }
 
+bool set_485_baud_rate(byte baud_code) {
+#if !COMMON_SERIAL_PORT
+	if (baud_code == 0 || my_address == UNIT_DISABLED) {
+		EEPROM.write(EE_ADDR_485_SPEED, 0);
+		return true;
+	}
+	if ((baud_code >= '0' && baud_code <= '9') 
+	|| (baud_code >= 'A' && baud_code <= 'C')
+	|| (baud_code >= 'a' && baud_code <= 'c')) {
+		EEPROM.write(EE_ADDR_485_SPEED, baud_code);
+		start_485_serial();
+		return true;
+	}
+#endif
+	return false;
+}
+
 // begin: start off the state machine
 void CommandStateMachine::begin(void) {
+	source_485 = false;
 	reset();
 }
 
 // reset: reset the state machine back to the start state
 void CommandStateMachine::reset(void) {
+#if COMMON_SERIAL_PORT
+	source_485 = false;	/* simulate transition when we don't have a dedicated RS-485 USART */
+#endif
 	state = IdleState;
+	address_count = 0;
 	command_in_progress = 0;
+	next_byte_literal = false;
+	next_byte_msb = false;
 	buffer_idx = 0;
 	LEDset = 0;
 	for (int i=0; i<CSM_BUFSIZE; i++) 
@@ -501,24 +612,121 @@ void CommandStateMachine::reset(void) {
 
 
 // accept an input character into the state machine
-void CommandStateMachine::accept(int inputchar) {
+void CommandStateMachine::accept(int inputchar, bool from_485) {
 	int i;
+
+#if COMMON_SERIAL_PORT
+	// If we only have one USART, we'll take a MSB=1 byte as an
+	// indicator that we're receiving RS-485 protocol stuff. We
+	// will clear that when we reset the state machine so if we
+	// receive another one, we'll set it again but if we receive
+	// a command without the RS-485 header we'll say that's a USB
+	// command.
+	if (inputchar & 0x80) {
+		from_485 = true;
+	}
+#endif
+
+	// if we were in the middle of a command and suddenly flipped
+	// input source, signal that as an error
+	if (from_485 && !source_485) {			// switching to RS-485
+		if (state != IdleState) {
+			error();
+		}
+		source_485 = true;
+	} else if (!from_485 && source_485) {	// switching to USB
+		if (state != IdleState) {
+			error();
+		}
+		source_485 = false;
+	}
 
 	if (inputchar < 0) {
 		return;
 	}
 
-	switch (state) {
-		case EndState:
-		case ErrorState:
+	// If in End or Error state, wait for command boundary then move to Idle state.
+	if (state == EndState || state == ErrorState) {
+		if (source_485) {
+			if (inputchar & 0x80) {
+				reset();
+				/* continue to interpret this byte below */
+			}
+			else {
+				return;
+			}
+		} else {
 			if (inputchar == '\x04') {
-				// ^D marks end of command; reset to start next command
 				reset();
 			}
-			// Otherwise stay here until a ^D arrives
-			break;
+			return;
+		}
+	}
 
+	if (source_485 && (inputchar & 0x80) && state != IdleState) {
+		// encountered sudden start of command
+		error();
+		reset();
+		// continue to interpret this byte below 
+	}
+
+	// Handle start of RS-485 command block
+	if (source_485) {
+		byte target;
+		if (state == IdleState) {
+			if (inputchar & 0x80) {
+				/* start of new command */
+				if ((target = (inputchar & 0x0f)) == my_address || target == global_address) {
+					if ((target & 0x70) == 0x00) {	// 1000aaaa: turn off all lights
+						all_off(true);
+						end_cmd();
+						return;
+					}
+					if ((target & 0x70) == 0x10) {	// 1001aaaa: normal command addressed to us (or global)
+						state = StartState;
+						return;
+					}
+					if ((target & 0x70) == 0x30) {	// 1011aaaa: start of multi-address block
+						state = CollectNState;
+						return;
+					}
+					error();
+					return;
+				} else {
+					/* not addressed to us */
+					end_cmd();
+					return;
+				}
+			} else {
+				/* data byte while waiting for start of command */
+				end_cmd();
+				return;
+			}
+		} else {
+			/* RS-485 byte received while working on command; apply escape codes */
+			if (next_byte_literal) {
+				next_byte_literal = false;
+			} else {
+				if (next_byte_msb) {
+					next_byte_msb = false;
+					inputchar |= 0x80;
+				} else {
+					if (inputchar == 0x7e) {
+						next_byte_msb = true;
+						return;
+					}
+					if (inputchar == 0x7f) {
+						next_byte_literal = true;
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	switch (state) {
 		case IdleState:
+		case StartState:
 			// Start of command
 			switch (command_in_progress = inputchar) {
 				case '*': 
@@ -568,17 +776,46 @@ void CommandStateMachine::accept(int inputchar) {
 			break;
 
 		case SetState:
-			// ignore input byte (for readerboard compatibility)
+			if (inputchar == '_') {
+				my_address = UNIT_DISABLED;
+			} else if ((my_address = (inputchar - '0')) < 0 || my_address > 15) {
+				error();
+				break;
+			}
+			EEPROM.write(EE_ADDR_485_ADDR, my_address);
 			state = SetUSpeedState;
 			break;
 
-		case SetUSpeedState:
-			if (set_baud_rate(inputchar)) {
-				end_cmd();
-			} else {
+		case SetAgState:
+			if (inputchar == '_') {
+				global_address = UNIT_DISABLED;
+			} else if ((global_address = (inputchar - '0')) < 0 || global_address > 15) {
 				error();
+				break;
 			}
+			EEPROM.write(EE_ADDR_GLOB_ADDR, global_address);
 
+			if (!set_baud_rate(buffer[0])) {
+				error();
+				break;
+			}
+#if !COMMON_SERIAL_PORT
+			if (!set_485_baud_rate(buffer[1])) {
+				error();
+				break;
+			}
+#endif
+			end_cmd();
+			break;
+
+		case SetUSpeedState:
+			buffer[0] = inputchar;
+			state = SetRSpeedState;
+			break;
+
+		case SetRSpeedState:
+			buffer[1] = inputchar;
+			state = SetAgState;
 			break;
 
 		case FlashState:
@@ -600,6 +837,35 @@ void CommandStateMachine::accept(int inputchar) {
 				set_steady_led(buffer[0]);
 			}
 			end_cmd();
+			break;
+
+		case CollectNState:
+			if (inputchar == 0) {
+				state = StartState;
+			} else if (inputchar > 15) {
+				error();
+			} else {
+				state = CollectAddressState;
+				address_count = inputchar;
+			}
+			break;
+
+		case CollectAddressState:
+			if (!append_buffer(inputchar)) {
+				error();
+				break;
+			}
+			if (--address_count == 0) {
+				for (i=0; i<buffer_idx; i++) {
+					if ((buffer[i] & 0x0f) == my_address || (buffer[i] & 0x0f) == global_address) {
+						buffer_idx = 0;
+						state = StartState;
+						return;
+					}
+				}
+				end_cmd();
+				return;
+			}
 			break;
 
 		default:
@@ -671,8 +937,13 @@ void loop() {
 	strober.update();
 
 	if (Serial.available() > 0) {
-		csm.accept(Serial.read());
+		csm.accept(Serial.read(), false);
 	}
+#if !COMMON_SERIAL_PORT
+	if (Serial1.available() > 0) {
+		csm.accept(Serial1.read(), true);
+	}
+#endif
 }
 
 // flash a single light on/off, or sequence through a list at the on cadence.
